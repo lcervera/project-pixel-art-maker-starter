5372a478bc0ac43d280face50e5ec810e4627011
DESKTOP-VB46K1L\Louisa <louisa.cervera@gmail.com>
2018-09-22 22:38:16 -0500

    final project

--- designs.js
+++ designs.js
@@ -3,8 +3,34 @@
 
 // When size is submitted by the user, call makeGrid()
 
+$(document).ready(init);
+
 function makeGrid() {
 
-// Your code goes here!
+    var height = $("#inputHeight").val();
+    var width = $("#inputWidth").val();
+    var grid = $("#pixelCanvas");
 
+    $("tr", grid).remove();
+
+    for (var i = 1; i <= height; i++) {
+        var myTr = $("<tr>");
+        for (var j = 1; j <= width; j++) {
+            var myTd = $("<td>");
+            myTd.click(paint)
+            myTr.append(myTd);
+        }
+        grid.append(myTr);
+    }
 }
+
+function paint() {
+    var color = $("#colorPicker").val();
+    $(this).css('background-color', color);
+}
+
+function init() {
+    $("#buttonMakeGrid").click(makeGrid);
+}
+
+
